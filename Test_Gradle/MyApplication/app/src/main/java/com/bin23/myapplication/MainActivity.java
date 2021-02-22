4ccc9abb8c5f9b074d9cb4c22c41aac734374dfe
God23Bin <479017580@qq.com>
2021-02-22 23:19:55 +0800

    测试Gradle，安卓需要

---
+++ Test_Gradle/MyApplication/app/src/main/java/com/bin23/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bin23.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
