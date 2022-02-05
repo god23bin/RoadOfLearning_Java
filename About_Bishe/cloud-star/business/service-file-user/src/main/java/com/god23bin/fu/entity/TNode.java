b53f59dec7122ac892a7b11b515c2ccce728f48f
god23bin <479017580@qq.com>
2022-02-06 00:30:18 +0800

    实体类的编写，使用了Lombok和Swagger的注解，以及几个MP的注解 - VO - File - TNode 树结点 - User - UserDirectory

---
+++ About_Bishe/cloud-star/business/service-file-user/src/main/java/com/god23bin/fu/entity/TNode.java
@@ -0,0 +1,20 @@
+package com.god23bin.fu.entity;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(value="目录树结点", description="")
+public class TNode {
+    private long id;
+    private String name;
+    private long parentId;
+    private static int idCounter = 0;
+    private List<TNode> childrenList;
+}
