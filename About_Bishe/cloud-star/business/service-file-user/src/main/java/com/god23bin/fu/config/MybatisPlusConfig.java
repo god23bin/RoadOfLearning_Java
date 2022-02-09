90eb4a822a0b0912616c9757df5224cfb47d2af9
god23bin <479017580@qq.com>
2022-02-09 15:43:33 +0800

    MP 配置

---
+++ About_Bishe/cloud-star/business/service-file-user/src/main/java/com/god23bin/fu/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.god23bin.fu.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MP 配置
+ * @author god23bin
+ */
+@Configuration
+@MapperScan("com.god23bin.fu.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+}
