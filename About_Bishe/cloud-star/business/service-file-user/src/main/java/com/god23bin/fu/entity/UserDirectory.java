4967889d85670b5c6acce1cae0827c452502091c
god23bin <479017580@qq.com>
2022-02-12 16:05:05 +0800

    已将实体类抽取到commonbase模块中作为公共类

--- About_Bishe/cloud-star/business/service-file-user/src/main/java/com/god23bin/fu/entity/UserDirectory.java
+++ About_Bishe/cloud-star/business/service-file-user/src/main/java/com/god23bin/fu/entity/UserDirectory.java
@@ -1,32 +1,32 @@
-package com.god23bin.fu.entity;
-
-import java.io.Serializable;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
-
-/**
- * <p>
- * 用户目录结构表
- * </p>
- *
- * @author god23bin
- * @since 2022-01-24
- */
-@Data
-@EqualsAndHashCode(callSuper = false)
-@Accessors(chain = true)
-@ApiModel(value="UserDirectory对象", description="用户目录结构表")
-public class UserDirectory implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    private String userId;
-
-    @ApiModelProperty(value = "JSON格式的用户目录结构")
-    private String userDirStruct;
-
-
-}
+//package com.god23bin.fu.entity;
+//
+//import java.io.Serializable;
+//import io.swagger.annotations.ApiModel;
+//import io.swagger.annotations.ApiModelProperty;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.experimental.Accessors;
+//
+///**
+// * <p>
+// * 用户目录结构表
+// * </p>
+// *
+// * @author god23bin
+// * @since 2022-01-24
+// */
+//@Data
+//@EqualsAndHashCode(callSuper = false)
+//@Accessors(chain = true)
+//@ApiModel(value="UserDirectory对象", description="用户目录结构表")
+//public class UserDirectory implements Serializable {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    private String userId;
+//
+//    @ApiModelProperty(value = "JSON格式的用户目录结构")
+//    private String userDirStruct;
+//
+//
+//}
--- About_Bishe/cloud-star/business/service-file-user/src/main/java/com/god23bin/fu/entity/vo/FolderVo.java
+++ About_Bishe/cloud-star/business/service-file-user/src/main/java/com/god23bin/fu/entity/vo/FolderVo.java
@@ -1,26 +1,26 @@
-package com.god23bin.fu.entity.vo;
-
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-/**
- * 封装文件夹实体类
- * @author god23bin
- */
-@Data
-@AllArgsConstructor
-public class FolderVo {
-
-    @ApiModelProperty(value = "文件夹ID")
-    private String id;
-
-    @ApiModelProperty(value = "文件夹所属用户ID")
-    private String userId;
-
-    @ApiModelProperty(value = "文件夹名字")
-    private String name;
-
-    @ApiModelProperty(value = "父文件夹ID")
-    private long parentId;
-}
+//package com.god23bin.fu.entity.vo;
+//
+//import io.swagger.annotations.ApiModelProperty;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+///**
+// * 封装文件夹实体类
+// * @author god23bin
+// */
+//@Data
+//@AllArgsConstructor
+//public class FolderVo {
+//
+//    @ApiModelProperty(value = "文件夹ID")
+//    private String id;
+//
+//    @ApiModelProperty(value = "文件夹所属用户ID")
+//    private String userId;
+//
+//    @ApiModelProperty(value = "文件夹名字")
+//    private String name;
+//
+//    @ApiModelProperty(value = "父文件夹ID")
+//    private long parentId;
+//}
