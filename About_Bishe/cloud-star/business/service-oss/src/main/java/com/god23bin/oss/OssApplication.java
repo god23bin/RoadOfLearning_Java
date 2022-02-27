b390a86a92d104bb057baddb54aa50c73ad55492
god23bin <479017580@qq.com>
2022-02-27 14:45:11 +0800

    添加上@SpringBootApplication

--- About_Bishe/cloud-star/business/service-oss/src/main/java/com/god23bin/oss/OssApplication.java
+++ About_Bishe/cloud-star/business/service-oss/src/main/java/com/god23bin/oss/OssApplication.java
@@ -2,9 +2,9 @@ package com.god23bin.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.god23bin"})
 public class OssApplication {
     public static void main(String[] args) {
